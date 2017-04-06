423347991c7e2c08e6a3ad95fd062bd3bbd5f655
DruCmHc <cmhidalgo6@utpl.edu.ec>
2017-04-06 00:04:57 -0500

    Modify mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@
   config.action_mailer.smtp_settings = {
       address: 'smtp.gmail.com',
       port: 587,
-      domain: 'gmail.com',
+      domain: 'gestionvehicular.herokuapp.com',
       authentication: 'plain',
       enable_starttls_auto: true,
       user_name: ENV['MAILER'],
